49422ced06eec37c08a55bd194303c50a22326c6
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-09-05 23:27:32 +0500

    Update transaction_list.dart

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,51 +11,60 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemCount: UserTransactions.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: Row(
+      child: UserTransactions.isEmpty
+          ? Column(
               children: [
-                Container(
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 15,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Theme.of(context).primaryColor,
-                      width: 2,
-                    ),
-                  ),
-                  padding: const EdgeInsets.all(10),
-                  child: Text(
-                    '\$${UserTransactions[index].amount.toStringAsFixed(2)}',
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple,
-                    ),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(UserTransactions[index].title,
-                        style: Theme.of(context).textTheme.titleMedium),
-                    Text(
-                      DateFormat().format(UserTransactions[index].date),
-                      style: const TextStyle(
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
+                Text(
+                  'No trasanction added yet!',
+                  style: Theme.of(context).textTheme.titleMedium,
                 )
               ],
+            )
+          : ListView.builder(
+              itemCount: UserTransactions.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Theme.of(context).primaryColor,
+                            width: 2,
+                          ),
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          '\$${UserTransactions[index].amount.toStringAsFixed(2)}',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(UserTransactions[index].title,
+                              style: Theme.of(context).textTheme.titleMedium),
+                          Text(
+                            DateFormat().format(UserTransactions[index].date),
+                            style: const TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
